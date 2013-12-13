4c6f410d5b0d722ec31d4270269491abb3a206bd
Witold Mescheryakov <witcold@users.noreply.github.com>
2013-12-13 11:45:31 -0800

    Update UnitMatrix.pas
    Рефакторинг

--- UnitMatrix.pas
+++ UnitMatrix.pas
@@ -21,9 +21,9 @@ type
     StringGridResult: TStringGrid;
     ButtonTest: TButton;
     procedure FormCreate(Sender: TObject);
+    procedure GenerateA(var A: TMatrix; N, Min, Max: Integer; var y: TVector);
+    procedure Jacobi (n:integer; A:TMatrix; max_aij:TType; var k:integer; var avgL, meraR: TType; lambda:TVector);    
     procedure ButtonTestClick(Sender: TObject);
-    Procedure Jacobi (n:integer; A:TMatrix; max_aij:TType; var k:integer; var sr_oc_toch_L:TType; var sr_mera_toch_r:TType; lyambda:TVector);
-    procedure GenerateA(var A:TMatrix; n, min_L, max_L:integer; var y:TVector);
   private
     { Private declarations }
   public
@@ -66,17 +66,7 @@ begin
     setlength (a[i], n);
 end;
 
-function MultiVect(A,B: TVector; n:integer): TMatrix;
-var
-  i, j: Integer;
-begin
-  InitM(Result,n);
-  for i := 0 to n-1 do
-    for j := 0 to n-1 do
-      Result[i,j]:=A[i]*B[j];
-end;
-
-function trans(a: TMatrix; n: Integer): TMatrix;
+function Transpose(a: TMatrix; n: Integer): TMatrix;
 var 
   i, j:integer;
 begin
@@ -86,7 +76,7 @@ begin
       Result[i,j] := a[j,i];
 end;
 
-function MultiMatr(A,B: TMatrix; n:integer): TMatrix;
+function MultiplyMatrix(A,B: TMatrix; n:integer): TMatrix;
 var
   i, j, k: Integer;
 begin
@@ -99,7 +89,7 @@ begin
     end;
 end;
 
-procedure Upor(var M: TVector; n: Integer);
+procedure Sort(var M: TVector; n: Integer);
 var 
   i, j: integer;
   t: TType;
@@ -113,7 +103,7 @@ begin
       end;
 end;
 
-procedure TFormMain.GenerateA(var A:TMatrix; n, min_L, max_L:integer; var y:TVector);
+procedure TFormMain.GenerateA(var A: TMatrix; N, Min, Max: Integer; var y: TVector);
 var
   i,j: integer;
   x: TVector;
@@ -123,37 +113,37 @@ begin
   //генерация собственных значений лямбда
   SetLength(y,n);
   randomize;
-  for i:=0 to n-1 do
-    y[i]:=random(max_L)-min_L+random;
+  for i := 0 to n-1 do
+    y[i] := Min + Random * (Max - Min);
 
-  Upor (y,n);
+  Sort(y,n);
 
   //диагональная матрица лямбда, на ее диагонали стоят собственные значения
-  InitM(diag,n);
+  InitM(diag, n);
   for i:=0 to n-1 do
     diag [i,i]:=y[i];
 
   //генерация и нормирование вектора X
   SetLength(x, n);
-  sum:=0;
-  for i:=0 to n-1 do begin
-    x[i]:=random(5)+1;
-    sum:=sum + x[i]*x[i];
+  sum := 0;
+  for i := 0 to n-1 do begin
+    x[i] := random(5)+1;
+    sum := sum + x[i]*x[i];
   end;
   sum := 1/sqrt(sum);
-  for i:=0 to n-1 do
-    x[i]:=x[i]*sum;
+  for i := 0 to n-1 do
+    x[i] := x[i]*sum;
 
   //получение матрицы Хаусхолдера
   InitM(H,n);
-  for i:=0 to n-1 do
-    for j:=0 to n-1 do
+  for i := 0 to n-1 do
+    for j := 0 to n-1 do
      if i = j then
-       H[i,j]:=1-2*X[i]*X[j]
+       H[i,j] := 1 - 2*X[i]*X[j]
      else
-       H[i,j]:=-2*X[i]*X[j];
+       H[i,j] := -2*X[i]*X[j];
   //получение матрицы А
-  A := MultiMatr(MultiMatr(H,diag,n), trans(H,n),n);
+  A := MultiplyMatrix(MultiplyMatrix(H,diag,n), Transpose(H,n),n);
 end;
 
 procedure Method (var Bpred, Bk, T:TMatrix; n:integer);
@@ -186,19 +176,19 @@ begin
     s := c;
   end;
 
-  Bk[imax,imax]:=sqr(c)*Bpred[imax,imax]+sqr(s)*Bpred[jmax,jmax]+2*c*s*Bpred[imax,jmax];
-  Bk[jmax,jmax]:=sqr(s)*Bpred[imax,imax]+sqr(c)*Bpred[jmax,jmax]-2*c*s*Bpred[imax,jmax];
+  Bk[imax,imax] := sqr(c)*Bpred[imax,imax]+sqr(s)*Bpred[jmax,jmax]+2*c*s*Bpred[imax,jmax];
+  Bk[jmax,jmax] := sqr(s)*Bpred[imax,imax]+sqr(c)*Bpred[jmax,jmax]-2*c*s*Bpred[imax,jmax];
 
-  Bk[imax,jmax]:=0;
-  Bk[jmax,imax]:=0;
+  Bk[imax,jmax] := 0;
+  Bk[jmax,imax] := 0;
 
   //проверка
-  pr:=(sqr(c)-sqr(s))*Bpred[imax, jmax]+c*s*(Bpred[jmax, imax]-Bpred[imax,imax]);
+  pr := (sqr(c)-sqr(s))*Bpred[imax, jmax]+c*s*(Bpred[jmax, imax]-Bpred[imax,imax]);
 
-  for i:=0 to n-1 do
-    if (i<>imax) and (i<>jmax) then begin
+  for i := 0 to n-1 do
+    if (i <> imax) and (i <> jmax) then begin
       Bk[imax, i] := c*Bpred[i,imax]+s*Bpred[i,jmax];
-      Bk[i, imax] := Bk[imax,i];
+      Bk[i, imax] := Bk[imax, i];
       Bk[jmax, i] := -s*Bpred[i,imax]+c*Bpred[i,jmax];
       Bk[i, jmax] := Bk[jmax, i];
     end;
@@ -215,7 +205,7 @@ begin
   Tnew[imax, jmax] := -s;
   Tnew[jmax, jmax] := c;
   Tnew[jmax, imax] := s;
-  T := MultiMatr(T, Tnew, n);
+  T := MultiplyMatrix(T, Tnew, n);
 end;
 
 procedure Pogresh (lambda, sobst:TVector; var pogr: TType; n: Integer);
@@ -228,13 +218,13 @@ begin
       pogr := abs(lambda[i] - sobst[i]);
 end;
 
-Procedure TFormMain.Jacobi (n:integer; A:TMatrix; max_aij:TType; var k:integer; var sr_oc_toch_L, sr_mera_toch_r:TType; lyambda:TVector);
+Procedure TFormMain.Jacobi (n: integer; A:TMatrix; max_aij: TType; var k: integer; var avgL, meraR: TType; lambda:TVector);
 var
   i,j: Integer;
-  M:integer;
-  T:TMatrix;
-  sobst:TVector;
-  max:TType;
+  M: integer;
+  T: TMatrix;
+  sobst: TVector;
+  max: TType;
   Bpred, bk, diag, TD,AT:TMatrix;
   
   // Проверка, является ли матрица диагональной
@@ -256,7 +246,7 @@ var
   end;
 
 begin
-  M:=10000;
+  M := 10000;
   //первоначально единичная матрица
   InitM(T, n);
   for i := 0 to n-1 do
@@ -270,9 +260,10 @@ begin
       Bk[i,j]:=A[i,j];
     end;
 
+  k := 0;
   while not DiagMatr (Bpred, n) and (k <= M) do begin
     Method (BPred, Bk, T, n);
-    k:=k+1;
+    Inc(k);
     for i:=0 to n-1 do
       for j:=0 to n-1 do
         Bpred[i,j]:=Bk[i,j];
@@ -288,46 +279,42 @@ begin
   InitM(TD, n);
   InitM(AT, n);
 
-  TD:=MultiMatr(T,diag, n);
-  AT:=MultiMatr(A,T, n);
-  max:=abs(TD[0,0]-AT[0,0]);
-  for i:=0 to n-1 do
-    for j:=0 to n-1 do
-      if abs(TD[i,j]-AT[i,j])>max then max:=abs(TD[i,j]-AT[i,j]);
-  sr_mera_toch_r:=max;
+  TD := MultiplyMatrix(T, diag, n);
+  AT := MultiplyMatrix(A, T, n);
+  max := abs(TD[0,0]-AT[0,0]);
+  for i := 0 to n-1 do
+    for j := 0 to n-1 do
+      if abs(TD[i,j]-AT[i,j]) > max then
+        max := abs(TD[i,j]-AT[i,j]);
+  meraR := max;
 
-  Upor (sobst, n);
-  pogresh (lyambda, sobst,sr_oc_toch_L, n);
+  Sort(sobst, n);
+  pogresh(lambda, sobst, avgL, n);
 end;
 
 procedure TFormMain.ButtonTestClick(Sender: TObject);
 var
   i, j, k, t, line, iterations: integer;
-  sr_oc_toch_L, sr_mera_toch_r: TType;
+  avgL, mera, sr_oc_toch_L, sr_mera_toch_r: TType;
   lambda: TVector;
-  matr_oc_L, matr_mera: TType;
-  matr_k: integer;
+  matr_k: Integer;
 begin
   line := 1;
   for i := 1 to Length(Ranks) do
     for j := 1 to Length(Ranges) do
       for k := 1 to Length(Epsilons) do begin
         iterations := 0;
-        sr_oc_toch_L:=0;
-        sr_mera_toch_r:=0;
+        sr_oc_toch_L := 0;
+        sr_mera_toch_r := 0;
         for t := 1 to Tests do begin
-          matr_k:=0;
-          matr_oc_L:=0;
-          matr_mera:=0;
-          InitM(A, Ranks[i]);
           GenerateA(A, Ranks[i], -Ranges[j], Ranges[j], lambda);
-          Jacobi(Ranks[i], A, Epsilons[k],matr_k,matr_oc_L, matr_mera, lambda);
-          sr_oc_toch_L:=sr_oc_toch_L+matr_oc_L;
-          sr_mera_toch_r:=sr_mera_toch_r+matr_mera;
+          Jacobi(Ranks[i], A, Epsilons[k], matr_k, avgL, mera, lambda);
+          sr_oc_toch_L := sr_oc_toch_L + avgL;
+          sr_mera_toch_r := sr_mera_toch_r + mera;
           iterations := iterations + matr_k;
         end;
-        sr_oc_toch_L:=sr_oc_toch_L/Tests;
-        sr_mera_toch_r:=sr_mera_toch_r/Tests;
+        sr_oc_toch_L := sr_oc_toch_L/Tests;
+        sr_mera_toch_r := sr_mera_toch_r/Tests;
         iterations := round(iterations / Tests);
         with StringGridResult do begin
           Cells[1, line] := IntToStr(Ranks[i]);
@@ -338,7 +325,7 @@ begin
           Cells[6, line] := FloatToStr(sr_mera_toch_r);
         end;
         Application.ProcessMessages;
-        line := line + 1;
+        Inc(line);
       end;
 end;
 
